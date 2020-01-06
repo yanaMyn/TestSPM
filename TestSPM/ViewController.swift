849f75eca1bdac64299bb222a18eeed74a51c491
yana mulyana <yanamyn15@gmail.com>
2020-01-06 22:22:45 +0700

    Initial Commit

---
+++ TestSPM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestSPM
+//
+//  Created by yana mulyana on 06/01/20.
+//  Copyright © 2020 LinkAja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
